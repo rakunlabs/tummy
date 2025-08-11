199d83279ff047dfd0957d1a224ec12204ed0bf6
Eray Ates <eates23@gmail.com>
2025-08-12 00:38:47 +0200

    fix: update mutex
    Signed-off-by: Eray Ates <eates23@gmail.com>

--- example_test.go
+++ example_test.go
@@ -10,6 +10,8 @@ import (
 func Example() {
 	tummy.Enable()
 
+	tummy.Resume() // it is already resuming
+
 	tummy.SetTime(time.Date(1919, 5, 19, 12, 0, 0, 0, time.UTC))
 
 	tummy.AddDuration(2 * time.Hour)
@@ -18,7 +20,22 @@ func Example() {
 	tummy.AddDate(0, 0, 1)
 	fmt.Println(tummy.Now().Day()) // 20
 
+	tummy.Pause()
+	tummy.SetTime(time.Date(1923, 10, 23, 12, 0, 0, 0, time.UTC))
+
+	fmt.Println(tummy.Now().Format(time.RFC3339Nano) == "1923-10-23T12:00:00Z") // true
+
+	tummy.Pause()
+	fmt.Println(tummy.IsPaused()) // true
+	tummy.Resume()
+
+	tummy.Disable()                                                                  // reset tummy.Now to time.Now
+	fmt.Println(tummy.Now().Format(time.RFC3339) == time.Now().Format(time.RFC3339)) // true
+
 	// Output:
 	// 14
 	// 20
+	// true
+	// true
+	// true
 }
--- tummy.go
+++ tummy.go
@@ -1,6 +1,7 @@
 package tummy
 
 import (
+	"sync"
 	"time"
 )
 
@@ -12,12 +13,12 @@ var (
 
 // Enable sets the global time.Now to a custom tummy.Now function.
 func Enable() {
-	Now = global.Now
+	global.Enable()
 }
 
 // Disable resets the global tummy.Now to the original time.Now function.
 func Disable() {
-	Now = time.Now
+	global.Disable()
 }
 
 // AddDuration adds a duration to the global tummy.Now.
@@ -35,33 +36,117 @@ func SetTime(t time.Time) {
 	global.SetTime(t)
 }
 
+// Pause pauses the global tummy.Now, freezing the time.
+func Pause() {
+	global.Pause()
+}
+
+// Resume resumes the global tummy.Now, unfreezing the time.
+func Resume() {
+	global.Resume()
+}
+
+// IsPaused checks if the global tummy.Now is currently paused.
+func IsPaused() bool {
+	return global.IsPaused()
+}
+
 // ////////////////////////////////////
 
 type tummy struct {
-	Start time.Time
-	Time  time.Time
+	start time.Time
+	time  time.Time
+
+	paused     bool
+	pausedTime time.Time
+
+	m sync.RWMutex
 }
 
 func new() *tummy {
 	return &tummy{
-		Start: time.Now(),
-		Time:  time.Now(),
+		start: time.Now(),
+		time:  time.Now(),
 	}
 }
 
+func (t *tummy) Enable() {
+	t.m.Lock()
+	defer t.m.Unlock()
+
+	Now = t.Now
+}
+
+func (t *tummy) Disable() {
+	t.m.Lock()
+	defer t.m.Unlock()
+
+	Now = time.Now
+}
+
 func (t *tummy) SetTime(t2 time.Time) {
-	t.Start = time.Now()
-	t.Time = t2
+	t.m.Lock()
+	defer t.m.Unlock()
+
+	t.start = time.Now()
+	t.time = t2
+
+	if t.paused {
+		t.pausedTime = t.start
+	}
 }
 
 func (t *tummy) Now() time.Time {
-	return t.Time.Add(time.Since(t.Start))
+	t.m.RLock()
+	defer t.m.RUnlock()
+
+	if t.paused {
+		return t.time.Add(t.pausedTime.Sub(t.start))
+	}
+
+	return t.time.Add(time.Since(t.start))
 }
 
 func (t *tummy) AddDuration(d time.Duration) {
-	t.Time = t.Time.Add(d)
+	t.m.Lock()
+	defer t.m.Unlock()
+
+	t.time = t.time.Add(d)
 }
 
 func (t *tummy) AddDate(years, months, days int) {
-	t.Time = t.Time.AddDate(years, months, days)
+	t.m.Lock()
+	defer t.m.Unlock()
+	t.time = t.time.AddDate(years, months, days)
+}
+
+func (t *tummy) Pause() {
+	t.m.Lock()
+	defer t.m.Unlock()
+
+	if t.paused {
+		return
+	}
+
+	t.pausedTime = time.Now()
+	t.paused = true
+}
+
+func (t *tummy) Resume() {
+	t.m.Lock()
+	defer t.m.Unlock()
+
+	if !t.paused {
+		return
+	}
+
+	t.start = t.start.Add(time.Since(t.pausedTime))
+	t.paused = false
+}
+
+func (t *tummy) IsPaused() bool {
+	t.m.RLock()
+	defer t.m.RUnlock()
+
+	return t.paused
 }
